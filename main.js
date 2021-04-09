64d7013c6706e68b2d3b425a7b65affabf4521f2
Serhiioc <kreat1vesc2gmail.com>
2021-04-10 02:01:08 +0300

    i wins

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const playerObj = {
-        player1: {
+
+    const $arenas = document.querySelector('.arenas');
+    const $randomBtn = document.querySelector('.button');
+    const player1 =  {
+            player: 1,
             name: 'Scorpion',
             hp: '100',
             img: 'scorpion.gif',
@@ -8,57 +11,87 @@ document.addEventListener("DOMContentLoaded", function() {
             attack: function() {
                 console.log(player1.name + ' Fight...')
             },
-        },
-        player2: {
+        };
+    const player2 =  {
+            player: 2,
             name: 'KItana',
-            hp: '80',
+            hp: '100',
             img: 'kitana.gif',
             weapon: ['katana', 0],
             attack: function() {
                 console.log(player2.name + ' Fight...')
             },
-        },
-
     };
 
+    
 
-    const $arenas = document.querySelector(".arenas");
+    function createElement (tag, className) {
+        const $tag = document.createElement(tag);
+
+        if (className) {
+            $tag.classList.add(className);
+        }
+        return $tag
+    }
    
-    function createPlayer(player, name, src) {
+    function createPlayer(playerObj) {
         // ! Player div
-        const $players = document.createElement('div');
-        $players.classList.add(player);
+        const $players = createElement('div', 'player'+playerObj.player);
         // ! Progress
-        const $progressbar = document.createElement('div');
-        $progressbar.classList.add('progressbar');
+        const $progressbar = createElement('div', 'progressbar');
         // * Life
-        const $life = document.createElement('div');
-        $life.classList.add('life');
-        $life.style.width = '100%';
+        const $life = createElement('div', 'life');
+        $life.style.width = playerObj.hp+'%';
         // * Name
-        const $name = document.createElement('div');
-        $name.classList.add('name');
-        $name.innerText = name;
+        const $name = createElement('div', 'name');
+        $name.innerText = playerObj.name;
 
         $progressbar.appendChild($life);
         $progressbar.appendChild($name);
         // ! CHaracter
-        const $character = document.createElement('div');
-        $character.classList.add('character');
+        const $character = createElement('div', 'character');
         // * Image
-        const $img = document.createElement('img');
-        $img.classList.add('img');
-        $img.src = 'http://reactmarathon-api.herokuapp.com/assets/' + src;
+        const $img = createElement('img');
+        $img.src = 'http://reactmarathon-api.herokuapp.com/assets/' + playerObj.img;
 
         $character.appendChild($img);
 
         $players.appendChild($progressbar);
         $players.appendChild($character);
 
-        $arenas.appendChild($players);
+       return $players;
 
     };
+    $arenas.appendChild(createPlayer(player1));
+    $arenas.appendChild(createPlayer(player2));
 
-        createPlayer('player1', playerObj.player1.name, playerObj.player1.img);
-        createPlayer('player2', playerObj.player2.name, playerObj.player2.img);
+    function playerWins(name) {
+        const $winsTitle = createElement('div', 'loseTitle');
+        $winsTitle.innerText = name + ' wins';
+
+        return $winsTitle;
+    }
+ 
+    function changeHp (player) {
+        const $playerLife = document.querySelector('.player'+ player.player + ' .life');
+        player.hp -= Math.ceil(Math.random() * 20);
+        $playerLife.style.width = player.hp + '%';
+
+        if (player.hp <= 0) {
+            $playerLife.style.width = '0';
+            $randomBtn.setAttribute("disabled", "1");
+            $arenas.appendChild(playerWins(player1.hp > 0? player1.name : player2.name));
+        }
+
+        
+        }
+        $randomBtn.addEventListener('click', function() {
+            changeHp(player1);
+            changeHp(player2);
+            
+        });
+    
+    
+
+   
 });
